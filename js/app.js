cccc0e1e7ec2739e52ff7dd6352100564309a255
Joshua Harris <joshua.jharris13@gmail.com>
2015-10-16 20:50:29 -0400

    added angular carousel, needs styling

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 var app = angular.module("oduacm", ["firebase", "ui.bootstrap"]);
 
+app.controller("CarouselController", function($scope) {
+  $scope.myInterval = 5000;
+  $scope.noWrapSlides = false;
+  var slides = $scope.slides = [];
+  slides.push({
+    image: 'public/img/jumbotron.jpg',
+    text: 'ODU ACM'
+  });
+  slides.push({
+    image: 'public/img/jumbotron.jpg',
+    text: 'ODU ACM Slide 2'
+  });
+});
+
+
 app.controller("EventsController", function($scope, $firebaseArray) {
 
   var ref = new Firebase("https://oduacm.firebaseio.com/events");
@@ -11,7 +26,7 @@ app.controller("EventsController", function($scope, $firebaseArray) {
     _.forEach(events, function(element) {
       console.log(element);
     });
-  
+
   });
 
 });
