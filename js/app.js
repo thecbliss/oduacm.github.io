02c0f7f0dbddfc6b58d11139371425717550d611
Joshua Harris <joshua.jharris13@gmail.com>
2015-10-19 21:08:50 -0400

    added person count to events pane

--- js/app.js
+++ js/app.js
@@ -23,8 +23,14 @@ app.controller("EventsController", function($scope, $firebaseArray) {
 
   $scope.events.$loaded().then(function(events){
 
+    function getAttendees(commaAttendees){
+      if(commaAttendees) {return commaAttendees.split(',');}
+      else {return [];}
+    }
     _.forEach(events, function(element) {
-      console.log(element);
+      //console.log(events.$getRecord(element.$id));
+      events.$getRecord(element.$id).attendees = getAttendees(element.attendeesStr);
+      events.$getRecord(element.$id).numAttending = element.attendees.length;
     });
 
   });
